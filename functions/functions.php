503e25d1f0f51ab45e942326dd9cdaecbc853846
Hidehito Nozawa aka Suin <suinyeze@gmail.com>
2012-05-29 17:26:09 +0900

    add isHiragana()

--- functions/functions.php
+++ functions/functions.php
@@ -11,106 +11,155 @@
  *
  */
 
-
-/**
- * コマンドラインで標準入力を取得する関数
- * @link http://suin.asia/2011/04/04/how_to_pass_stdin_to_php
- */
-function getStdin()
+class Suin_PhpFunctions
 {
-	return file_get_contents('php://stdin');
-}
-
-/**
- * glob()を再帰的に行う
- * @link http://suin.asia/2011/02/25/recursive_glob_method_php
- */
-function rglob($pattern = '*', $flags = 0, $path = '')
-{
-	$paths = glob($path.'*', GLOB_MARK|GLOB_ONLYDIR|GLOB_NOSORT);
-	$files = glob($path.$pattern, $flags);
-
-	foreach ( $paths as $path )
+	/**
+	 * コマンドラインで標準入力を取得する関数
+	 * @link http://suin.asia/2011/04/04/how_to_pass_stdin_to_php
+	 * @return string
+	 */
+	public function getStdin()
 	{
-		$files = array_merge($files, rglob($pattern, $flags, $path));
+		return file_get_contents('php://stdin');
 	}
 
-	return $files;
-}
-
-/**
- * パスワード生成関数を1行で
- * @link http://suin.asia/2010/12/08/php_password_generate_function
- */
-function make_password($chars = 8)
-{
-	return substr(str_shuffle('1234567890qwertyuiopasdfghjklzxcvbnm'), 0, $chars);
-}
-
-/**
- * 正規表現にマッチするかどうがboolで返す
- */
-function is_match($pattern, $subject)
-{
-	return ( preg_match($pattern, $subject) > 0 );
-}
-
-/**
- * 正規表現置換に失敗したらNOTICEを出す
- */
-
-function regex_replace($pattern, $replacement, $subject)
-{
-	$string = preg_replace($pattern, $replacement, $subject)
-
-	if ( $string === null )
+	/**
+	 * glob()を再帰的に行う
+	 * @link http://suin.asia/2011/02/25/recursive_glob_method_php
+	 * @param string $pattern
+	 * @param int $flags
+	 * @param string $path
+	 * @return array
+	 */
+	public function rglob($pattern = '*', $flags = 0, $path = '')
 	{
-		trigger_error('preg_replace: failed to replace.', E_USER_NOTICE);
+		$paths = glob($path.'*', GLOB_MARK|GLOB_ONLYDIR|GLOB_NOSORT);
+		$files = glob($path.$pattern, $flags);
+
+		foreach ( $paths as $path )
+		{
+			$files = array_merge($files, rglob($pattern, $flags, $path));
+		}
+
+		return $files;
 	}
 
-	return $string;
-}
-
-/**
- * 連想配列をKey-Value配列に変換する
- */
-function hashToKeyValue(array $data, $keyName, $valueName)
-{
-	$keyValueData = array();
-
-	foreach ( $data as $datum )
+	/**
+	 * パスワード生成関数を1行で
+	 * @link http://suin.asia/2010/12/08/php_password_generate_function
+	 * @param int $chars
+	 * @return string
+	 */
+	public function makePassword($chars = 8)
 	{
-		$key   = $datum[$keyName];
-		$value = $datum[$valueName];
-		$keyValueData[$key] = $value;
+		return substr(str_shuffle('1234567890qwertyuiopasdfghjklzxcvbnm'), 0, $chars);
 	}
 
-	return $keyValueData;
-}
-
-/**
- * オブジェクトの名前空間を返す
- */
-function get_namespace($obj)
-{
-	$className = get_class($obj);
-	$className = strtr($className, '\\', '/');
-	$className = dirname($className);
-	$namespace = strtr($className, '/', '\\');
-	return $namespace;
-}
-
-/**
- * キーストレッチしてより解読困難なパスワードハッシュを生成する
- */
-function stretch($password, $salt, $stretch = 100000)
-{
-	$key = '';
-
-	for ( $i = 0; $i < $stretch; $i++ )
+	/**
+	 * 正規表現にマッチするかどうがboolで返す
+	 * @param $pattern
+	 * @param $subject
+	 * @return bool
+	 */
+	public function isMatch($pattern, $subject)
 	{
-		$key = md5($key.$password.$salt);
+		return ( preg_match($pattern, $subject) > 0 );
 	}
 
-	return $key;
+	/**
+	 * 正規表現置換に失敗したらNOTICEを出す
+	 * @param $pattern
+	 * @param $replacement
+	 * @param $subject
+	 * @return mixed
+	 */
+	public function regexReplace($pattern, $replacement, $subject)
+	{
+		$string = preg_replace($pattern, $replacement, $subject);
+
+		if ( $string === null )
+		{
+			trigger_error('preg_replace: failed to replace.', E_USER_NOTICE);
+		}
+
+		return $string;
+	}
+
+	/**
+	 * 連想配列をKey-Value配列に変換する
+	 * @param array $data
+	 * @param $keyName
+	 * @param $valueName
+	 * @return array
+	 */
+	public function hashToKeyValue(array $data, $keyName, $valueName)
+	{
+		$keyValueData = array();
+
+		foreach ( $data as $datum )
+		{
+			$key   = $datum[$keyName];
+			$value = $datum[$valueName];
+			$keyValueData[$key] = $value;
+		}
+
+		return $keyValueData;
+	}
+
+	/**
+	 * オブジェクトの名前空間を返す
+	 * @param $obj
+	 * @return string
+	 */
+	public function getNamespace($obj)
+	{
+		$className = get_class($obj);
+		$className = strtr($className, '\\', '/');
+		$className = dirname($className);
+		$namespace = strtr($className, '/', '\\');
+		return $namespace;
+	}
+
+	/**
+	 * キーストレッチしてより解読困難なパスワードハッシュを生成する
+	 * @param $password
+	 * @param $salt
+	 * @param int $stretch
+	 * @return string
+	 */
+	public function stretch($password, $salt, $stretch = 100000)
+	{
+		$key = '';
+
+		for ( $i = 0; $i < $stretch; $i++ )
+		{
+			$key = md5($key.$password.$salt);
+		}
+
+		return $key;
+	}
+
+	/**
+	 * 文字列がひらがなかを返す
+	 *
+	 * 次の文字以外にマッチした場合、FALSEを返します:
+	 * ぁ あ ぃ い ぅ う ぇ え ぉ お か が き ぎ く ぐ け げ こ ご さ ざ し じ す ず せ ぜ そ ぞ
+	 * た だ ち ぢ っ つ づ て で と ど な に ぬ ね の は ば ぱ ひ び ぴ ふ ぶ ぷ へ べ ぺ ほ ぼ
+	 * ぽ ま み む め も ゃ や ゅ ゆ ょ よ ら り る れ ろ ゎ わ ゐ ゑ を ん ゔ ゕ ゖ ー
+	 *
+	 * @param string $string
+	 * @return bool
+	 */
+	public function isHiragana($string)
+	{
+		$hiragana = '\xe3(\x81[\x81-\xbf]|\x82[\x80-\x96])'; // [ぁ-み]|[む-ゖ]
+		$chonpu   = '\xe3\x83\xbc'; // ー
+		$pattern  = "/^($hiragana|$chonpu)+$/";
+
+		if ( preg_match($pattern, $string) ) {
+			return true;
+		}
+
+		return false;
+	}
 }
--- test/functions/FunctionsTest.php
+++ test/functions/FunctionsTest.php
@@ -1,9 +1,47 @@
 <?php
 
+require_once dirname(dirname(__DIR__)).'/functions/functions.php';
+
 class Suin_PhpFunctions_FunctionsTest extends PHPUnit_Framework_TestCase
 {
-	public function testTest()
+	/**
+	 * @return Suin_PhpFunctions
+	 */
+	public function newFunctions()
 	{
-		$this->assertTrue(true);
+		return new Suin_PhpFunctions();
+	}
+
+	/**
+	 * @param $expect
+	 * @param $string
+	 * @dataProvider dataForTestIsHiragana
+	 */
+	public function testIsHiragana($expect, $string)
+	{
+		$function = $this->newFunctions();
+		$actual = $function->isHiragana($string);
+		$this->assertSame($expect, $actual);
+	}
+
+	public static function dataForTestIsHiragana()
+	{
+		return array(
+			// [expect, string]
+			array(true, "あ"),
+			array(true, "ん"),
+			array(true, "ぁ"),
+			array(true, "ゎ"),
+			array(true, "ー"),
+			array(true, "ぴーえっちっぴー"),
+			array(true, "ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんゔゕゖー"),
+
+			array(false, "。"),
+			array(false, "、"),
+			array(false, "ア"),
+			array(false, "ａ"),
+			array(false, "４"),
+			array(false, "ぴーえっちっぴー5.4"),
+		);
 	}
 }
